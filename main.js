87bc953a967d00398b489f42fe810cad57c0cae0
alaina-khan <92048635+alaina-khan@users.noreply.github.com>
2021-11-17 20:37:53 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+function setup() {
+    canvas= createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function clearCanvas()
+{
+    background("white");
+}
+
+function preload() {
+    classifier= ml5.imageClassifier('DoodleNet');
+}
+function draw() {
+//Set stroke weight to 13
+strokeWeight(13);
+//Set stroke color to black
+stroke(0);
+// If mouse is pressed, draw line between previous and current mouse position
+if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+}
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML= 'Label:' + results[0].label;
+
+    document.getElementById('confidence').innerHTML= 'Confidence:' + Math.round(results[0].confidence*100) + '%' ;
+
+    utterThis= new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
+canvas
+{
+    border:5px solid white;
+    border-radius: 20 px;
+    animation-name: example;
+    animation-duration: 4s;
+    margin-top: 50px;
+    animation-iteration-count:infinite;
+
+
+}
+
+    @keyframes example {
+        from {box-shadow: 1px 1px 38px Grey;}
+        to {box-shadow: 1px 1px 38px White;}
+
+    }
+
+    p
+    {
+        padding-top: 10px;
+        font-size: 25px;
+    
+    }
